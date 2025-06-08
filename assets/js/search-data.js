df8fc9eb14c4b9fc710d4d3aaf1e132efa2de489
wyhallenwu <67626395+wyhallenwu@users.noreply.github.com>
2025-06-08 09:51:13 +0000

    Deploying to gh-pages from @ wyhallenwu/wyhallenwu.github.io@a324c6fe71917d60f49f598cf54da33777c5dcdd 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,60 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "ABOUT",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "PUBLICATIONS",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
+          title: "PROJECTS",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "news-langcoop-is-accepted-by-cvpr-2nd-workshop-on-multi-agent-embodied-intelligent-systems-2025-thanks-xiangbo-and-all-collaborators-check-our-paper-and-code",
+          title: 'Langcoop is accepted by CVPR 2nd Workshop on Multi-Agent Embodied Intelligent Systems 2025....',
+          description: "",
+          section: "News",},{id: "projects-langcoop",
+          title: 'Langcoop',
+          description: "CVPR Workshop, 2025",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/langcoop.html";
+            },},{id: "projects-octopinf",
+          title: 'OCTOPINF',
+          description: "Percom, 2024",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/octopinf.html";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%75%68%65%6E%67%77%75(%61%74)%6B%61%69%73%74.%61%63.%6B%72", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=bAhIqYwAAAAJ", "_blank");
+        },
+      },];
